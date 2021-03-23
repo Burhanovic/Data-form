149f0e33ec2bca65659572734cdb7e1e1f19ea42
Burhanovic <burhanaljoundi@gmail.com>
2021-03-23 14:02:12 +0100

    data

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+
+
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App/>
   </Router>,
   document.getElementById('root')
